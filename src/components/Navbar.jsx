5f5f105321a892ddea074ca8ca1419d30133f485
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-02 20:40:33 +0700

    import search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,9 @@
+import { Search } from "@mui/icons-material";
 import { Stack } from "@mui/system";
 import { Link } from "react-router-dom";
 
 import { logo } from "../utils/constants";
+import SearchBar from "./SearchBar";
 
 const Navbar = () => (
   <Stack
@@ -18,6 +20,7 @@ const Navbar = () => (
     <Link to="/" style={{ display: "flex", alignItems: "center" }}>
       <img src={logo} alt="logo" height={45} />
     </Link>
+    <SearchBar />
   </Stack>
 );
 
