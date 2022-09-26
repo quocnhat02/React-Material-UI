03c56860e0dcd48a9e793a889f9d8865e6fc5e28
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 19:44:48 +0700

    modern ui/ux 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import {} from 'react-icons';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <h1>GPT-3</h1>
+    </div>
+  );
 };
 
 export default App;
