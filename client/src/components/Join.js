a18d8bf40e6c94cc373ab7528f385efe5642f8cc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 12:07:17 +0700

    socket 12

--- client/src/components/Join.js
+++ client/src/components/Join.js
@@ -5,7 +5,34 @@ const Join = () => {
   const [name, setName] = useState('');
   const [room, setRoom] = useState('');
 
-  return <div>Join</div>;
+  return (
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div>
+          <input
+            type="text"
+            placeholder=""
+            className="joinInput"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </div>
+        <div>
+          <input
+            type="text"
+            placeholder=""
+            className="joinInput mt-20"
+            onChange={(event) => setRoom(event.target.value)}
+          />
+        </div>
+        <Link>
+          <button className="button mt-20" type="submit">
+            Sign In
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Join;
