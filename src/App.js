55f7dfd5209631dbc99307edf556a45b3c8bd6e7
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 20:03:59 +0700

    modern ui/ux 8

--- src/App.js
+++ src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
 
-import Article from './components/article/Article';
-import Brand from './components/brand/Brand';
-import CTA from './components/cta/CTA';
-import Feature from './components/feature/Feature';
-import Navbar from './components/navbar/Navbar';
-
 const App = () => {
   return (
     <div>
