f7f0ce398f3ed949cd0a953cda03c89bf21280bc
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 19:07:06 +0700

    socket 22

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -3,10 +3,14 @@ import queryString from 'query-string';
 import io from 'socket.io-client';
 
 const Chat = ({ location }) => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
   useEffect(() => {
     const { name, room } = queryString.parse(location.search);
 
-    console.log(name, room);
+    setName(name);
+    setRoom(room);
   });
 
   return <div>Chat</div>;
