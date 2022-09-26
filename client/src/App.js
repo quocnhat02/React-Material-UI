06af005336341b9ec5a44fe841e1e044b4361571
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 11:43:00 +0700

    socket 4

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Join from '../components/Join';
+import Chat from '../components/Chat';
+
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" exact element={<Join />} />
+      <Route path="/chat" element={<Chat />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+export default App;
