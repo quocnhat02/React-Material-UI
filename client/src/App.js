c3f39cea74c6425ecf819cb5cf8776789ae3f7c1
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 18:58:09 +0700

    socket 18

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Join from './components/Join';
-import Chat from './components/Chat';
+import Join from './components/Join/Join';
+import Chat from './components/Chat/Chat';
 
 const App = () => (
   <BrowserRouter>
