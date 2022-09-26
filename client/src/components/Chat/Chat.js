bcc5d3b4a419d4080a411776a93d23fe9c66b598
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 19:03:35 +0700

    socket 20

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -2,7 +2,14 @@ import React, { useState, useEffect } from 'react';
 import queryString from 'query-string';
 import io from 'socket.io-client';
 
-const Chat = () => {
+const Chat = ({ location }) => {
+  useEffect(() => {
+    const data = queryString.parse(location.search);
+
+    console.log(location.search);
+    console.log(data);
+  });
+
   return <div>Chat</div>;
 };
 
