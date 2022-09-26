887f04cf5c39ea30cfb35b3ad6958bdc8dd120c6
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 19:26:27 +0700

    clear

--- server/router.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => {
-  return res.send('Server is up and running');
-});
-
-module.exports = router;
