d441c9ac3b6c33ad74fae8811368c49d40dbe8b4
nguyenquocnhat <nguyenquocnhat12b1@gmail.com>
2022-09-26 20:01:20 +0700

    modern ui/ux 7

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react';
-import {} from 'react-icons';
+
+import Article from './components/article/Article';
+import Brand from './components/brand/Brand';
+import CTA from './components/cta/CTA';
+import Feature from './components/feature/Feature';
+import Navbar from './components/navbar/Navbar';
 
 const App = () => {
   return (
